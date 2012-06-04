aa3be2043cf7f5a989702f839abf0b829a3e06b0
Dear layone <superlayone@163.com>
2012-06-04 11:49:01 +0800

    Daily BKU

--- BMVS/BMVS.cpp
+++ BMVS/BMVS.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include <fstream>
+#include <cstdio>
+#include <cstdlib>
 using namespace std;
 #include "CPU_IncentiveDef.h"
 #include "IO_IncentiveDef.h"
 #include "MEM_IncentiveDef.h"
 #include "Share_IncentiveDef.h"
+ifstream fin("data.in");
 struct Request
 {
        double CPU;
@@ -14,6 +18,8 @@ struct Request
        remain
        */    
 };
+Request Req[200],FormattedReq[200];
+
 struct TaskList
 {
        int BelongedClass;
@@ -25,7 +31,7 @@ int PA=0,PB=0,PC=0,PD=0;
 double Profit=0;
 double _class[4]={0.16,0.05,0.09,0.7};
 int BelongedTo[1000]={1};
-void Bmvs2FetchRequest(int ReqNum,Request *Req,Request *FormattedReq)
+void Bmvs2FetchRequest(int ReqNum,Request Req[],Request FormattedReq[])
 {
      /*
      int CPU=Max(Req)-Min(Req);
@@ -39,7 +45,7 @@ void Bmvs2FetchRequest(int ReqNum,Request *Req,Request *FormattedReq)
      }
      */
 }
-void Classify(Request *FormattedReq)
+void Classify(Request FormattedReq[])
 {
      /*class训练集，data测试数据输入，并利用分类器对data数据分类，cMat2是分类后的混淆矩阵
      O2 = NaiveBayes.fit(data,class,'dist',{'kernel','normal','kernel'});
@@ -47,22 +53,23 @@ void Classify(Request *FormattedReq)
      cMat2 = confusionmat(class,C2);
      */
 }
-void Bmvs2Classify(int ReqNum,Request *FormattedReq)
+void Bmvs2Classify(int ReqNum,Request FormattedReq[])
 {
      //Bmvs2FetchDatabase();  /*get history data*/
 	 Classify(FormattedReq);
      for(int i=0;i<ReqNum;i++)
      {
-             FormattedReq[i].BelongedClass=BelongedTo[i];
+             //FormattedReq[i].BelongedClass=BelongedTo[i];
      }
 }
-void Bmvs2Sched(int ReqNum,Request *FormattedReq)
+void Bmvs2Sched(int ReqNum,Request FormattedReq[])
 {
      int CountA=0,CountB=0,CountC=0,CountD=0;
      for(int i=0;i<ReqNum;i++)
      {
-             if(FormattedReq[i].BelongedClass==1)
-             {
+            /* 
+			if(FormattedReq[i].BelongedClass==1)
+            {
                     A[CountA++].BelongedClass=i;
              }
              /*else if()
@@ -124,8 +131,45 @@ void Bmvs2CMPP(double Profit)
 	}
     /*else return*/
 }
+void InitReq(int ReqNum)
+{
+	//freopen("data.in","r",stdin);
+	for(int i=0;i<ReqNum;i++)
+	{
+		fin>>Req[i].CPU;
+		fin>>Req[i].MEM;
+		fin>>Req[i].IO;
+		fin>>Req[i].BelongedClass;
+		fin>>FormattedReq[i].CPU;
+		fin>>FormattedReq[i].MEM;
+		fin>>FormattedReq[i].IO;
+		fin>>FormattedReq[i].BelongedClass;
+		
+	}
+
+}
 int main()
 {
-    
+	
+	int ReqNum=100;
+	int TaskNum=100;
+	cout<<"Importing request data!!"<<endl;
+	InitReq(ReqNum);
+    Bmvs2FetchRequest(ReqNum,Req,FormattedReq);
+	cout<<"Fetching request"<<endl;
+	cout<<"Fetching database"<<endl;
+	Bmvs2Classify(ReqNum,FormattedReq);
+	cout<<"Making classification"<<endl;
+	Bmvs2Sched(ReqNum,FormattedReq);
+	cout<<"Starting scheduling now"<<endl;
+	Bmvs2OptSched();
+	cout<<"Scheduling opt start now and deploy tasks request!"<<endl;
+	Bmvs2FetchP();
+	Bmvs2ComputeP(4,TaskNum);
+	cout<<"Fetch profit from database and compute P!"<<endl;
+	Bmvs2CMPP(Profit);
+	cout<<"If matched conditions,do storage exchange!"<<endl;
+
+	system("pause");
     return 0;
 }
